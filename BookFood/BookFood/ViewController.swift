669fda0a5da85dcbad01e9a0f996869700e9ce1f
dionysiuss <Dennis@DennisdeMacBook-Pro.local>
2017-05-17 12:34:17 +0800

    UI

---
+++ BookFood/BookFood/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BookFood
+//
+//  Created by jiarou on 2017/5/3.
+//  Copyright © 2017年 teamFour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet weak var TapReastaurant: UIButton!
+    @IBOutlet weak var TapMenber: UIButton!
+    @IBOutlet weak var TapFoodMap: UIButton!
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
