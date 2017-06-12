16c2b76f7b7f1d3f07118b7ab0fc8ca311862e3f
jiarou <102306007@nccu.edu.tw>
2017-06-12 15:32:37 +0800

    add firebase SopLIst

--- BookFood/BookFood/RestaurantViewController.swift
+++ BookFood/BookFood/RestaurantViewController.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
+import Firebase
 import FirebaseDatabase
 
-class RestaurantViewController: UIViewController {
+
+
+class RestaurantViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
      var ref: DatabaseReference!
      var shop_number = 0
      var shop_name = [String]()
+     var RestaurantInfo = [Shop]()
+     var refHandle: UInt!
+   
+    
+    @IBOutlet weak var mycollection: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(gerfirebaseShoname())
+         ref = Database.database().reference()
+               getfirebaseDATA()
         
-    }
     
+    }
+
     func gerfirebaseShoname() -> Array<Any>  {
         ref = Database.database().reference().child("Restuarant")
         ref.observe(DataEventType.value, with: { (snapshot) in
@@ -52,18 +62,42 @@ class RestaurantViewController: UIViewController {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 1//Menuimge.count
+        return  RestaurantInfo.count//Menuimge.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RestaurantCell", for: indexPath) as! RestaurantCollectionViewCell
-        
         // Configure the cell
-       // cell.MenuView.image = UIImage(named:Menuimge[indexPath.row])
-        //cell.MoneyView.text = ItemMony[indexPath.row]
-        //cell.ItemView.text = ItemName[indexPath.row]
+        do{
+        cell.Restaurantimg.image = UIImage(data: try NSData(contentsOf:NSURL(string: RestaurantInfo[indexPath.row].shop_img!)! as URL) as Data)
+        }catch let error{
+          NSLog("got an error creating the request: \(error)")
+        }
+   //     cell.MoneyView.text = ItemMony[indexPath.row]
+
+             cell.RestaurantName.text =  self.RestaurantInfo[indexPath.row].name
+   
+       
         //cell.StorageView.text = ItemStorage[indexPath.row]
         return cell
+    }
+    func getfirebaseDATA() {
+        refHandle = ref.child("Restuarant").observe(.childAdded, with: { (snapshot) in
+            if let dictionry = snapshot.value as? [String : AnyObject] {
+                var shop = Shop()
+                shop.setValuesForKeys(dictionry)
+                self.RestaurantInfo.append(shop)
+                print(self.RestaurantInfo)
+                DispatchQueue.main.async {
+                self.mycollection?.reloadData()
+                 print(self.RestaurantInfo)
+                    // Depends if you were populating a collection view or table view
+                }
+                
+            }
+        })
+         self.RestaurantInfo = []
+        
     }
 
     
---
+++ BookFood/BookFood/Restaurant_Info.swift
@@ -0,0 +1,18 @@
+//
+//  Restaurant_Info.swift
+//  BookFood
+//
+//  Created by jiarou on 2017/6/11.
+//  Copyright © 2017年 teamFour. All rights reserved.
+//
+
+
+import UIKit
+
+class Shop: NSObject{
+    var name: String?
+    var location: String?
+    var shop_img: String?
+
+
+}
